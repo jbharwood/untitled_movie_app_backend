4d8d499e8b4e02d07ed5c104a7a2c1176ab02077
jbharwood <josephblakeharwood@gmail.com>
2019-05-03 14:04:32 -0400

    zev is the man

--- app/serializers/game_instance_serializer.rb
+++ app/serializers/game_instance_serializer.rb
@@ -1,15 +1,23 @@
 class GameInstanceSerializer < ActiveModel::Serializer
-  attributes :id, :game_round, :game_status, :user_status, :game_questions, :question
+  attributes :id, :game_round, :game_status, :user_status, :game_questions
 
+  def game_questions
+    ActiveModel::SerializableResource.new(object.game_questions, each_serializer:GameQuestionSerializer)
+end
   # def question
   #   object.game_questions.question.map do |question|
   #     question
   #   end
   # end
-  def question
-    object.game_questions.question.map do |question|
-      question
-    end
-  end
+  # def question
+  #   object.game_questions.question.map do |question|
+  #     question
+  #   end
+  # end
+  # def answers
+  #   object.questions.map do |answer|
+  #     answer
+  #   end
+  # end
 
 end
